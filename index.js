b91a7ea2ce6d48404cb06b884aa2b6553a37dfa6
helloagabriele <133674472+helloagabriele@users.noreply.github.com>
2024-08-12 23:09:37 +0000

    12/08/2024

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const idade=17;
+let idade=18;
 const anoNascimento= 2006;
 let anoAtual=2024;
+const nomeCompleto="helloa";
+console.log(" meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento + " e no ano atual " + anoAtual+ " completo "+ idade + " anos  ");
 
 anoAtual=anoAtual+1;
 console.log(anoAtual);
 
+idade = idade +1;
+console.log(idade);
