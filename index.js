415664cf825ad0c44f27774ae7f1df25448d429f
helloagabriele <133674472+helloagabriele@users.noreply.github.com>
2024-09-23 23:05:26 +0000

    23/09/2024

--- index.js
+++ index.js
@@ -28,10 +28,18 @@ console.log(listaDeDestinos);
 if(idadeComprador >=18) {
  console.log ("olá senhor (a) " + nomeComprador);
   console.log("comprador maior de idade");
-console.log("destino disponiveis: "+ listaDeDestinos);
+ console.log("Escolha teu destino");
+
+  var contador= 0;
+  while(contador <9 ){
+  console.log(listaDeDestinos[contador] );
+    contador++;
+    
+  }
+  var destino = entrada("digite o numero que corresponde ao seu destino selecionado(0-8): ");
+  console.log(`destino selecionados foi: ${listaDeDestinos[destino]})`);
 }else{
   console.log("não foi possivel concluir  compra");
 console.log("voce é menor de idade");
 }
-
 
