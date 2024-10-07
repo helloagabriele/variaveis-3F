6643d13499e3488d06f94306899c716e299898b0
helloagabriele <133674472+helloagabriele@users.noreply.github.com>
2024-10-07 23:08:09 +0000

    07/10/2024

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const entrada = prompt();
 
 
 //agencia de viagens  hawaii paris nova york rio de janeiro toquio 
+console.log(" ")
+console.log("__________________________________________");
+console.log("          ☯  agencia de viagem  ☯         ");
+console.log("__________________________________________");
+console.log("  " );
+
 const listaDeDestinos= new Array(
   " Hawaii",
   " paris",
@@ -14,25 +20,27 @@ const listaDeDestinos= new Array(
   " rio de janeiro",
 "tóquio"
 );
-var nomeComprador = entrada("Digite seu nome: ");
-var idadeComprador= entrada("qual sua idade?");
-console.log(listaDeDestinos);
-console.log(listaDeDestinos[3]);
-listaDeDestinos.push("Coronel vivida");
-listaDeDestinos.push(" fraiburgo")
-listaDeDestinos.push("são paulo")
-listaDeDestinos.push("londres")
-console.log(listaDeDestinos);
-const loginCerto = "helloa";
+
+const loginCerto = "hello";
 const senhaCerta = 123;
 
-var login = entrada("qual é o seu login? ");
-var senha = entrada("Digite a sua senha: ")
+var login = entrada("Dados pessoais? ");
+var senha = entrada("senha de acesso: ")
 while(login != loginCerto || senha != senhaCerta){
 console.log ("Login ou senha incorretos");
   login=entrada("qual é o seu login?");
   senha=entrada("Digite a sua senha: ");
 }
+
+var nomeComprador = entrada("Digite seu nome: ");
+var idadeComprador= entrada("qual sua idade?");
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[3]);
+listaDeDestinos.push("Coronel vivida" );
+listaDeDestinos.push(" fraiburgo")
+listaDeDestinos.push("são paulo")
+listaDeDestinos.push("londres")
+console.log(listaDeDestinos);
 
 if(idadeComprador >=18) {
  console.log ("olá senhor (a) " + nomeComprador);
@@ -48,7 +56,7 @@ if(idadeComprador >=18) {
   var destino = entrada("digite o numero que corresponde ao seu destino selecionado(0-8): ");
   console.log(`destino selecionados foi: ${listaDeDestinos[destino]})`);
 }else{
-  console.log("não foi possivel concluir  compra");
-console.log("voce é menor de idade");
+  console.log("não foi possivel efetuar a compra");
+console.log("não permitido comprar sem a maior idade");
 }
 
